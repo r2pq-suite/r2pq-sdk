5323b85cf188c1a0fbd21b76cc8ea748d6a66420
Eric Newman <160673537+unbeatablegrowth@users.noreply.github.com>
2025-11-10 15:20:43 -0500

    Update register.go

--- crypto/pq/register.go
+++ crypto/pq/register.go
@@ -1,3 +1,5 @@
 package pq
 
-import _ "github.com/r2pq-suite/r2pq-crypto/shim"
+// Placeholder: downstream apps that want crypto shims should import
+// github.com/r2pq-suite/r2pq-crypto (which performs its own init/registration).
+// Keeping this file so package pq compiles cleanly without external deps.
